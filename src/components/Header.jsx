f7117c6283d8154776fbbcfdb7fb7bfa76de285d
天舟 <t@bytebase.com>
2022-10-05 01:12:29 +0800

    docs: reset

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ export function Header() {
         <div className="order-first -mx-4 flex flex-auto basis-full overflow-x-auto whitespace-nowrap border-b border-blue-600/10 py-4 font-mono text-xl text-slate-900 sm:-mx-6 lg:order-none lg:mx-0 lg:basis-auto lg:border-0 lg:py-0">
           <div className="mx-auto flex items-center gap-4 px-4">
             <p>
-              <time dateTime="2022-11-05">051</time>-
+              <time dateTime="2022-11-05">05</time>-
               <time dateTime="2022-11-06">06 of Nov, 2022</time>
             </p>
             <DiamondIcon className="h-1.5 w-1.5 overflow-visible fill-current stroke-current" />
