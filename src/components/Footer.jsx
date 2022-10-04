d504e0eecfced4d6824536492d210c4da7a93c28
天舟 <t@bytebase.com>
2022-10-04 23:47:01 +0800

    docs: add devjoy logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export function Footer() {
   return (
     <footer className="py-16">
       <Container className="flex flex-col items-center justify-between md:flex-row">
-        <Logo className="h-12 w-auto text-slate-900" />
+        <Logo className="h-16 w-auto text-slate-900" />
         <p className="mt-6 text-base text-slate-500 md:mt-0">
           Copyright &copy; {new Date().getFullYear()} DevJoy. All rights
           reserved.
