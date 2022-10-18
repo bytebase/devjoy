2c2f93e9012160bed904344f53c1b252fc49c43a
Steven <stevenlgtm@gmail.com>
2022-10-19 07:47:36 +0800

    chore: update medias mobile style

--- components/Newsletter.tsx
+++ components/Newsletter.tsx
@@ -10,13 +10,13 @@ const Newsletter = () => {
         <div className="relative -mx-4 overflow-hidden bg-slate-900 py-16 px-4 sm:-mx-6 sm:px-6 md:mx-0 md:rounded-5xl md:px-16 xl:px-20">
           <div className="relative mx-auto grid max-w-2xl grid-cols-1 gap-x-24 gap-y-14 xl:max-w-none xl:grid-cols-2">
             <div className="my-auto">
-              <p className="font-display text-4xl font-medium tracking-tighter text-white sm:whitespace-nowrap sm:text-5xl">
+              <p className="font-display text-3xl font-medium tracking-tighter text-white sm:whitespace-nowrap sm:text-5xl">
                 关注公众号获取活动资讯
               </p>
-              <p className="mt-8 font-display text-2xl font-medium tracking-tighter text-white">
+              <p className="mt-8 font-display text-xl font-medium tracking-tighter text-white">
                 2022.11.05 ~ 06 10:30 ~ 18:30 (周六，周日)
               </p>
-              <p className="mt-2 font-display text-2xl font-medium tracking-tighter text-white">
+              <p className="mt-2 font-display text-xl font-medium tracking-tighter text-white">
                 上海静安区威海路 696 号 WeWork 中国旗舰店
               </p>
             </div>
